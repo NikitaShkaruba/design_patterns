26f356aea48663ad0e8fe322af7a5009749a2be2
Shkaruba Nikita <sh.nickita@list.ru>
2016-08-22 23:54:30 +0300

    Refactor "visitor" pattern

--- src/main/java/patterns/behavioral/visitor/Client.java
+++ src/main/java/patterns/behavioral/visitor/Client.java
@@ -1,14 +1,5 @@
 package patterns.behavioral.visitor;
 
-/**
- * Created by Nikita Shkaruba on 31.01.16.
- * <p>
- * My Contacts:
- * Email: sh.nickita@list.ru
- * GitHub: github.com/SigmaOne
- * Vk: vk.com/wavemeaside
- */
-
 public class Client {
     public static void main(String[] args) {
         ComputerPart computer = new Computer();
--- src/main/java/patterns/behavioral/visitor/parts/Keyboard.java
+++ src/main/java/patterns/behavioral/visitor/parts/Keyboard.java
@@ -1,15 +1,7 @@
 package patterns.behavioral.visitor.parts;
 
-import patterns.behavioral.visitor.ComputerPart;
 import patterns.behavioral.visitor.ComputerPartVisitor;
-/**
- * Created by Nikita Shkaruba on 31.01.16.
- * <p>
- * My Contacts:
- * Email: sh.nickita@list.ru
- * GitHub: github.com/SigmaOne
- * Vk: vk.com/wavemeaside
- */
+import patterns.behavioral.visitor.ComputerPart;
 
 public class Keyboard implements ComputerPart {
     @Override
